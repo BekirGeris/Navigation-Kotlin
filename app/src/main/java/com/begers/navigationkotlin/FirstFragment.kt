cf92331ba4cab290e983889991359148722ffc42
bekirgeris <bekirgeris@marun.edu.tr>
2021-12-07 11:06:40 +0300

    +use view binding +Switch between fragments using navigation

--- app/src/main/java/com/begers/navigationkotlin/FirstFragment.kt
+++ app/src/main/java/com/begers/navigationkotlin/FirstFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.R.id
+import androidx.navigation.Navigation
+import com.begers.navigationkotlin.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +19,18 @@ class FirstFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button1.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
     }
 
 }
